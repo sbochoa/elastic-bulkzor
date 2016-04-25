df983769e72e28725a60ef96cbd2e3ada69e2d30
sbochoa <sochoa@outlook.com.pe>
2016-04-25 00:06:01 -0500

    changed the object generic type to a class level

---
+++ src/Bulkzor.Tests/Processors/ChunkProcessorTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Bulkzor.Tests.Processors
+{
+    [TestFixture]
+    public class ChunkProcessorTests
+    {
+        [Test]
+        public void Test()
+        {
+            
+        }
+    }
+}
